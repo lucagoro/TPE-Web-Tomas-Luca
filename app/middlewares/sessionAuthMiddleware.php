6019b2e8a5735ff81b3a348a533d15aadb92e28b
Luca Gorosito <lucagoro04@gmail.com>
2024-10-18 19:11:23 -0300

    cambios realizados

---
+++ app/middlewares/sessionAuthMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+    function sessionAuthMiddleware($res) {
+        session_start();
+        if(isset($_SESSION['id_usuario'])) {
+            $res->usuario = new stdClass();
+            $res->usuario->id_usuario = $_SESSION['id_usuario'];
+            $res->usuario->usuario = $_SESSION['usuario'];
+            return;
+        } else {
+            header("Location: " . BASE_URL . "showLogin");
+        }
+    }
+?>
